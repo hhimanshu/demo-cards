97a605cdb4d7d09805dc4bd8943846339a89b40e
hhimanshu <harit.himanshu@gmail.com>
2018-07-25 18:39:14 -0400

    Failed commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {AsciiMathDelim} from "./AsciiMathDelim";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AsciiMathDelim />, document.getElementById('root'));
 registerServiceWorker();
